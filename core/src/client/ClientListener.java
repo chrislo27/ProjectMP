1ad35d8f1a195f1d26162832b5791823853880a3
chrislo27 <kinchrislo@gmail.com>
2015-03-22 12:14:21 -0700

    A tiny bit of packet handling

---
+++ core/src/client/ClientListener.java
@@ -0,0 +1,22 @@
+package client;
+
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import common.packet.Packet;
+
+
+public class ClientListener extends Listener{
+
+	@Override
+	public void received(Connection connection, Object obj){
+		if(obj instanceof Packet){
+			((Packet) obj).actionClient(connection);
+		}
+	}
+	
+	@Override
+	public void disconnected(Connection connection){
+		
+	}
+	
+}
---
+++ core/src/server/ServerListener.java
@@ -0,0 +1,21 @@
+package server;
+
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import common.packet.Packet;
+
+
+public class ServerListener extends Listener{
+
+	@Override
+	public void received(Connection connection, Object obj){
+		if(obj instanceof Packet){
+			((Packet) obj).actionServer(connection);
+		}
+	}
+	
+	@Override
+	public void disconnected(Connection connection){
+		
+	}
+}
