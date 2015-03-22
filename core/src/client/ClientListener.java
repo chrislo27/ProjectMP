6c00da993abd08329c12170348d4c55791d12562
chrislo27 <kinchrislo@gmail.com>
2015-03-22 12:25:07 -0700

    Added connected method override from Listener

--- core/src/client/ClientListener.java
+++ core/src/client/ClientListener.java
@@ -2,11 +2,17 @@ package client;
 
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
+
 import common.packet.Packet;
 
 
 public class ClientListener extends Listener{
 
+	@Override
+	public void connected(Connection connection){
+		
+	}
+	
 	@Override
 	public void received(Connection connection, Object obj){
 		if(obj instanceof Packet){
--- core/src/server/ServerListener.java
+++ core/src/server/ServerListener.java
@@ -8,6 +8,11 @@ import common.packet.Packet;
 public class ServerListener extends Listener{
 
 	@Override
+	public void connected(Connection connection){
+		
+	}
+	
+	@Override
 	public void received(Connection connection, Object obj){
 		if(obj instanceof Packet){
 			((Packet) obj).actionServer(connection);
