6a74fcf938a5124228b97d727838054054378b5a
chrislo27 <kinchrislo@gmail.com>
2015-03-26 20:38:57 -0700

    Added block faces
    Uses bit masking

--- core/src/projectmp/common/block/Block.java
+++ core/src/projectmp/common/block/Block.java
@@ -1,6 +1,16 @@
 package projectmp.common.block;
 
-
 public class Block {
+
+	public static class BlockFace {
+
+		public static final int NONE = 0x0;
+		public static final int ALL = 0xF;
+		public static final int UP = 0x1;
+		public static final int DOWN = 0x2;
+		public static final int LEFT = 0x4;
+		public static final int RIGHT = 0x8;
+		
+	}
 
 }
