fbf05492c335dad7c963dcbdc398718b16b08db0
chrislo27 <kinchrislo@gmail.com>
2015-03-28 09:01:04 -0700

    Added output for reason of kick at handshake

--- core/src/projectmp/common/packet/Packet0Handshake.java
+++ core/src/projectmp/common/packet/Packet0Handshake.java
@@ -5,49 +5,53 @@ import projectmp.server.ServerLogic;
 
 import com.esotericsoftware.kryonet.Connection;
 
+public class Packet0Handshake implements Packet {
 
-public class Packet0Handshake implements Packet{
-
-	
 	public static final int ACCEPTED = 1;
 	public static final int REQUEST = 2;
 	public static final int REJECTED = 3;
-	
+
 	int state = REQUEST;
 	public String version = "";
 	public String username = null;
 	String rejectReason = "unknown reason";
-	
-	public Packet0Handshake(){
-		
+
+	public Packet0Handshake() {
+
 	}
-	
-	private void reject(String reason, Packet0Handshake returner){
+
+	private void reject(String reason, Packet0Handshake returner) {
 		returner.state = REJECTED;
 		returner.rejectReason = reason;
 	}
-	
+
 	@Override
 	public void actionServer(Connection connection, ServerLogic logic) {
 		Packet0Handshake returner = new Packet0Handshake();
 		returner.state = ACCEPTED;
-		
-		if(!version.equalsIgnoreCase(Main.version)){
+
+		if (!version.equalsIgnoreCase(Main.version)) {
 			reject("Incompatible versions (server is " + Main.version + ")", returner);
-		}else if(username == null || username.equals("")){
+		} else if (username == null || username.equals("")) {
 			reject("Invalid username", returner);
-		}else if(logic.server.getConnections().length > logic.maxplayers){
+		} else if (logic.server.getConnections().length > logic.maxplayers) {
 			reject("Max player limit reached (limit: " + logic.maxplayers + ")", returner);
 		}
-		Main.logger.debug("server return handshake: " + returner.rejectReason);
+
+		if (returner.state == REJECTED) {
+			Main.logger.info("Kicked " + connection.toString() + " ("
+					+ connection.getRemoteAddressTCP().toString() + ") for: "
+					+ returner.rejectReason);
+		}
+
 		connection.sendTCP(returner);
 	}
 
 	@Override
 	public void actionClient(Connection connection, Main main) {
-		if(state == ACCEPTED){
+		if (state == ACCEPTED) {
 			main.setScreen(Main.GAME);
-		}else if(state == REJECTED){
+		} else if (state == REJECTED) {
 			Main.ERRORMSG.setMessage("Failed to connect:\n" + rejectReason);
 			main.setScreen(Main.ERRORMSG);
 			connection.close();
