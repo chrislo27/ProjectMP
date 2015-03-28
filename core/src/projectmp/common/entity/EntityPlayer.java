19500315ab1a2aa04aca44635b4d5d166037b3e3
chrislo27 <kinchrislo@gmail.com>
2015-03-28 09:10:53 -0700

    Added player class

---
+++ core/src/projectmp/common/entity/EntityPlayer.java
@@ -0,0 +1,20 @@
+package projectmp.common.entity;
+
+import projectmp.common.world.World;
+
+
+public class EntityPlayer extends Entity{
+
+	public EntityPlayer(World w, float posx, float posy) {
+		super(w, posx, posy);
+	}
+
+	@Override
+	public void prepare() {
+	}
+
+	@Override
+	public void render() {
+	}
+
+}
