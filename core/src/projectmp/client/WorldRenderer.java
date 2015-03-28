86644458a3a7c8064e88abbf1519e362b264a47c
chrislo27 <kinchrislo@gmail.com>
2015-03-27 21:18:10 -0700

    New world renderer

---
+++ core/src/projectmp/client/WorldRenderer.java
@@ -0,0 +1,29 @@
+package projectmp.client;
+
+import projectmp.common.Main;
+import projectmp.common.Settings;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+public class WorldRenderer {
+	
+	Main main;
+	SpriteBatch batch;
+	OrthographicCamera camera;
+	
+	public WorldRenderer(Main m){
+		main = m;
+		batch = main.batch;
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Settings.DEFAULT_WIDTH, Settings.DEFAULT_HEIGHT);
+	}
+
+	public void updateCameraAndSetMatrices() {
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+	}
+	
+}
