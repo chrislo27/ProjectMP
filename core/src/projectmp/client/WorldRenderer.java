f47f6c89fc44747bc02bace6af0ac68e1792b637
chrislo27 <kinchrislo@gmail.com>
2015-03-29 15:36:39 -0700

    Culling of blocks

--- core/src/projectmp/client/WorldRenderer.java
+++ core/src/projectmp/client/WorldRenderer.java
@@ -1,9 +1,11 @@
 package projectmp.client;
 
 import projectmp.common.Main;
+import projectmp.common.Settings;
 import projectmp.common.world.World;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 
 public class WorldRenderer {
 
@@ -23,8 +25,15 @@ public class WorldRenderer {
 	public void renderWorld() {
 		camera.update();
 		batch.begin();
-		for(int x = 0; x < world.sizex; x++){
-			for(int y = 0; y < world.sizey; y++){
+		
+		int prex = (int) MathUtils.clamp(((camera.camerax / World.tilesizex) - 1), 0f, world.sizex);
+		int prey = (int) MathUtils.clamp(((camera.cameray / World.tilesizey) - 1), 0f, world.sizey);
+		int postx = (int) MathUtils.clamp((camera.camerax / World.tilesizex) + 2
+				+ (Settings.DEFAULT_WIDTH / World.tilesizex), 0f, world.sizex);
+		int posty = (int) MathUtils.clamp((camera.cameray / World.tilesizey) + 2
+				+ (Settings.DEFAULT_HEIGHT / World.tilesizex), 0f, world.sizey);
+		for(int x = prex; x < postx; x++){
+			for(int y = posty; y >= prey; y--){
 				world.getBlock(x, y).render(this, x, y);
 			}
 		}
