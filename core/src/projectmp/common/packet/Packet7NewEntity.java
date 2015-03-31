84453a65cb1ad8689f47f151b1ab336e4acc16d5
chrislo27 <kinchrislo@gmail.com>
2015-03-30 18:32:23 -0700

    Working packets (new entity, removed entity)

--- core/src/projectmp/common/packet/Packet7NewEntity.java
+++ core/src/projectmp/common/packet/Packet7NewEntity.java
@@ -1,6 +1,7 @@
 package projectmp.common.packet;
 
 import projectmp.common.Main;
+import projectmp.common.entity.Entity;
 import projectmp.server.ServerLogic;
 
 import com.esotericsoftware.kryonet.Connection;
@@ -8,12 +9,15 @@ import com.esotericsoftware.kryonet.Connection;
 
 public class Packet7NewEntity implements Packet {
 
+	Entity e;
+	
 	@Override
 	public void actionServer(Connection connection, ServerLogic logic) {
 	}
 
 	@Override
 	public void actionClient(Connection connection, Main main) {
+		Main.GAME.world.entities.add(e);
 	}
 
 }
--- core/src/projectmp/common/packet/Packet8RemoveEntity.java
+++ core/src/projectmp/common/packet/Packet8RemoveEntity.java
@@ -8,12 +8,20 @@ import com.esotericsoftware.kryonet.Connection;
 
 public class Packet8RemoveEntity implements Packet {
 
+	long uuid;
+	
 	@Override
 	public void actionServer(Connection connection, ServerLogic logic) {
 	}
 
 	@Override
 	public void actionClient(Connection connection, Main main) {
+		for(int i = 0; i < Main.GAME.world.entities.size; i++){
+			if(Main.GAME.world.entities.get(i).uuid == uuid){
+				Main.GAME.world.entities.removeIndex(i);
+				return;
+			}
+		}
 	}
 
 }
