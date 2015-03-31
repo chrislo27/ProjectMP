fd49f542e02382e5f1e6def4801c4baa78f753fb
chrislo27 <kinchrislo@gmail.com>
2015-03-30 19:01:25 -0700

    whooosh

--- core/src/projectmp/common/entity/EntityPlayer.java
+++ core/src/projectmp/common/entity/EntityPlayer.java
@@ -1,6 +1,10 @@
 package projectmp.common.entity;
 
+import com.badlogic.gdx.graphics.Texture;
+
 import projectmp.client.WorldRenderer;
+import projectmp.common.Main;
+import projectmp.common.util.AssetMap;
 import projectmp.common.world.World;
 
 
@@ -18,6 +22,9 @@ public class EntityPlayer extends Entity{
 
 	@Override
 	public void render(WorldRenderer renderer) {
+		world.batch.draw(world.main.manager.get(AssetMap.get("airwhoosh"), Texture.class), visualX
+				* World.tilesizex - renderer.camera.camerax,
+				Main.convertY(visualY * World.tilesizey - renderer.camera.cameray) - 32);
 	}
 
 }
