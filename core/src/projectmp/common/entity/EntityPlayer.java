d37157da5322fe15697554807bcf1d0301d31e98
chrislo27 <kinchrislo@gmail.com>
2015-04-03 14:12:56 -0700

    Gotta go faster

--- core/src/projectmp/common/entity/EntityPlayer.java
+++ core/src/projectmp/common/entity/EntityPlayer.java
@@ -17,10 +17,10 @@ public class EntityPlayer extends Entity {
 
 	@Override
 	public void prepare() {
-		this.maxspeed = 5f;
+		this.maxspeed = 15f;
 		this.accspeed = maxspeed * 5f;
-		this.sizex = 0.5f;
-		this.sizey = 0.5f;
+		this.sizex = 1f;
+		this.sizey = 1f;
 		this.hasEntityCollision = true;
 	}
 
@@ -29,12 +29,12 @@ public class EntityPlayer extends Entity {
 		if (!world.isServer && Main.GAME.getPlayer() == this) {
 			world.batch.draw(world.main.manager.get(AssetMap.get("airwhoosh"), Texture.class), x
 					* World.tilesizex - renderer.camera.camerax,
-					Main.convertY(y * World.tilesizey - renderer.camera.cameray) + 32);
+					Main.convertY(y * World.tilesizey - renderer.camera.cameray));
 
 		} else {
 			world.batch.draw(world.main.manager.get(AssetMap.get("airwhoosh"), Texture.class),
 					visualX * World.tilesizex - renderer.camera.camerax,
-					Main.convertY(visualY * World.tilesizey - renderer.camera.cameray) + 32);
+					Main.convertY(visualY * World.tilesizey - renderer.camera.cameray));
 		}
 	}
 
