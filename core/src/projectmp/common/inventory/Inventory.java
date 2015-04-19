ace0321dbeb77307f47784fc1b3f4d8517fb408b
chrislo27 <kinchrislo@gmail.com>
2015-04-18 21:24:48 -0700

    Let's start the item inventory system

---
+++ core/src/projectmp/common/inventory/Inventory.java
@@ -0,0 +1,10 @@
+package projectmp.common.inventory;
+
+import com.badlogic.gdx.utils.Array;
+
+
+public class Inventory {
+
+	Array<Slot> slots = new Array<Slot>();
+	
+}
---
+++ core/src/projectmp/common/inventory/Slot.java
@@ -0,0 +1,11 @@
+package projectmp.common.inventory;
+
+
+public class Slot {
+
+	ItemStack item = new ItemStack(null, 0);
+	
+	int posx = 0;
+	int posy = 0;
+	
+}
