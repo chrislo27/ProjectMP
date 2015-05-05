a8ee2d72636e87ec25049d2855fb28ec400addda
chrislo27 <kinchrislo@gmail.com>
2015-05-04 20:16:08 -0700

    Leftovers from Stray removed

--- core/src/projectmp/common/util/KeyBinds.java
+++ core/src/projectmp/common/util/KeyBinds.java
@@ -5,10 +5,6 @@ import com.badlogic.gdx.Input.Keys;
 
 public class KeyBinds {
 	
-	public static final int AUGMENT_ACTIVATE = Keys.D;
-	public static final int AUGMENT_NEXT = Keys.SHIFT_LEFT;
-	public static final int AUGMENT_PREV = Keys.CONTROL_LEFT;
-	
 	public static final int MOVEMENT_JUMP = Keys.SPACE;
 	public static final int MOVEMENT_LEFT = Keys.LEFT;
 	public static final int MOVEMENT_RIGHT = Keys.RIGHT;
