a1110fdcd0f480812c243048ee47c131c9dc905c
chrislo27 <kinchrislo@gmail.com>
2015-05-05 18:41:44 -0700

    Added random long for inventories (WIP)

--- core/src/projectmp/common/inventory/Inventory.java
+++ core/src/projectmp/common/inventory/Inventory.java
@@ -2,12 +2,15 @@ package projectmp.common.inventory;
 
 import projectmp.common.util.MathHelper;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 
 
 public class Inventory {
 
 	Array<Slot> slots = new Array<Slot>();
+	
+	long uuid = MathUtils.random(Long.MIN_VALUE + 1, Long.MAX_VALUE - 1);
 	
 	public Slot getSlotAt(float x, float y){
 		for(Slot slot : slots){
