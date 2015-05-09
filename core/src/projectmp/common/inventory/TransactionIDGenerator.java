848a140007720ecfc28c18db7185a6b59e37b9f4
chrislo27 <kinchrislo@gmail.com>
2015-05-08 17:26:06 -0700

    Removed incorrect documentation

--- core/src/projectmp/common/inventory/TransactionIDGenerator.java
+++ core/src/projectmp/common/inventory/TransactionIDGenerator.java
@@ -9,20 +9,6 @@ public class TransactionIDGenerator {
 	
 	private static Random random = new Random(System.nanoTime());
 	
-	/**
-	 * Returns a random transaction ID that's essentially never going to happen again
-	 * <br>
-	 * It sets the seed to the current nanosecond time, then returns <br>
-	 * <code>
-	 * nextLong() * randomSign
-	 * </code>
-	 * <p>
-	 * 
-	 * The only way to make it return a value again is to change the computer clock
-	 * back and somehow re-call this method at the exact nanosecond.
-	 * 
-	 * @return random transaction ID
-	 */
 	public static long getNewTransactionID(){
 		return random.nextLong() * MathUtils.randomSign();
 	}
