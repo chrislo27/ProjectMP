ebed1430ddc3b8691f947ba0ac30758d9c0c6ed1
chrislo27 <kinchrislo@gmail.com>
2015-05-10 08:53:31 -0700

    Added SideOnly (annotation) and Side (enum)
    Similar to MinecraftForge's

---
+++ core/src/projectmp/common/util/sidedictation/Side.java
@@ -0,0 +1,16 @@
+package projectmp.common.util.sidedictation;
+
+
+public enum Side {
+	
+	/**
+	 * shows this is used only on the client side
+	 */
+	CLIENT,
+	
+	/**
+	 * shows this is used only on the server side
+	 */
+	SERVER;
+	
+}
---
+++ core/src/projectmp/common/util/sidedictation/SideOnly.java
@@ -0,0 +1,10 @@
+package projectmp.common.util.sidedictation;
+
+import java.lang.annotation.Inherited;
+
+
+public @interface SideOnly{
+
+	Side value();
+	
+}
