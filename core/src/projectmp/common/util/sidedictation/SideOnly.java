5a89ec90d01826e3bf6ca6155f08a9f6b2fa19cd
chrislo27 <kinchrislo@gmail.com>
2015-05-10 09:00:57 -0700

    Added @Retention(SOURCE) to SideOnly, also some documentation

--- core/src/projectmp/common/util/sidedictation/SideOnly.java
+++ core/src/projectmp/common/util/sidedictation/SideOnly.java
@@ -1,8 +1,17 @@
 package projectmp.common.util.sidedictation;
 
 import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
-
+/**
+ * Used to show what networking side (client or server) the target is on.
+ * <br>
+ * Purely visual, therefore it has @Retention(RetentionPolicy.SOURCE)
+ * 
+ *
+ */
+@Retention(RetentionPolicy.SOURCE)
 public @interface SideOnly{
 
 	Side value();
