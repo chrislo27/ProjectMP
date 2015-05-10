504c19c0464e92bc0b3b828f934067e16aa53a96
chrislo27 <kinchrislo@gmail.com>
2015-05-09 18:34:46 -0700

    Fixed #8 - more ToD support

--- core/src/projectmp/common/world/Time.java
+++ core/src/projectmp/common/world/Time.java
@@ -11,9 +11,9 @@ public class Time {
 	public int currentDayTicks = 0;
 
 	public final int ticksPerDay = Math.round(Main.TICKS * 30);
-	public final float daytimePercentage = 0.5f;
-	public final float eveningPercentage = 0.1f;
-	public final float nighttimePercentage = 0.4f;
+	public static final float daytimePercentage = 0.5f;
+	public static final float eveningPercentage = 0.1f;
+	public static final float nighttimePercentage = 0.4f;
 
 	public Time() {
 
@@ -36,27 +36,52 @@ public class Time {
 	}
 
 	public TimeOfDay getCurrentTimeOfDay() {
-		if (currentDayTicks <= (daytimePercentage * ticksPerDay)) {
-			return TimeOfDay.DAYTIME;
-		} else if (currentDayTicks <= (daytimePercentage * ticksPerDay)
-				+ (eveningPercentage * ticksPerDay)) {
-			return TimeOfDay.EVENING;
-		} else {
-			return TimeOfDay.NIGHTTIME;
-		}
+		return TimeOfDay.getTimeOfDayFromPercentage(currentDayTicks / (ticksPerDay * 1f));
 	}
 
 	public static enum TimeOfDay {
-		DAYTIME((byte) 127, Color.rgb888(0, 0, 0)), EVENING((byte) 64, Color.rgb888(1, 174 / 255f, 0)), NIGHTTIME(
-				(byte) 16, Color.rgb888(0, 0, 0));
+		DAYTIME((byte) 127, Color.rgb888(0, 0, 0), Time.daytimePercentage), EVENING((byte) 64,
+				Color.rgb888(1, 174 / 255f, 0), Time.eveningPercentage), NIGHTTIME((byte) 16, Color
+				.rgb888(0, 0, 0), Time.nighttimePercentage);
 
 		public byte lightLevel = 127;
 		public int color = Color.rgb888(0, 0, 0);
+		public float percentage = 0f;
 
-		private TimeOfDay(byte light, int color) {
+		private TimeOfDay(byte light, int color, float percent) {
 			lightLevel = light;
 			this.color = color;
+			percentage = percent;
 		}
+		
+		public static float getTotalPercentage(){
+			float f = 0;
+			
+			for(TimeOfDay t : TimeOfDay.values()){
+				f += t.percentage;
+			}
+			
+			return f;
+		}
+		
+		public static TimeOfDay getTimeOfDayFromPercentage(float percentage){
+			float totalPercentage = getTotalPercentage();
+			
+			float curper = 0;
+			TimeOfDay[] values = TimeOfDay.values();
+			for(int i = 0; i < values.length; i++){
+				TimeOfDay tod = values[i];
+				
+				if(percentage >= curper && percentage <= curper + tod.percentage){
+					return tod;
+				}
+				
+				curper += tod.percentage;
+			}
+			
+			return null;
+		}
+		
 	}
 
 }
