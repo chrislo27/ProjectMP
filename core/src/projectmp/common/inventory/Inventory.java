51621da49bb9bd590be9aa047d005e62fb13dcbe
chrislo27 <kinchrislo@gmail.com>
2015-05-10 20:52:06 -0700

    Changed from Array to... array

--- core/src/projectmp/common/inventory/Inventory.java
+++ core/src/projectmp/common/inventory/Inventory.java
@@ -8,16 +8,14 @@ import com.badlogic.gdx.utils.Array;
 
 public class Inventory {
 
-	Array<Slot> slots = new Array<Slot>();
+	public static final int PLAYER_SLOTS = 10;
 	
-	public Slot getSlotAt(float x, float y){
-		for(Slot slot : slots){
-			if(MathHelper.intersects(x, y, 1, 1, slot.posx, slot.posy, slot.width, slot.height)){
-				return slot;
-			}
-		}
-		
-		return null;
+	ItemStack[] items;
+	int maxCapacity;
+	
+	public Inventory(final int maxCap){
+		maxCapacity = maxCap;
+		items = new ItemStack[maxCap];
 	}
 	
 }
