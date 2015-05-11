af40ba98e467c14a25d6d509cf1baaabfdfa1736
chrislo27 <kinchrislo@gmail.com>
2015-05-10 20:42:04 -0700

    Gui classes for later

---
+++ core/src/projectmp/client/gui/GuiHandler.java
@@ -0,0 +1,14 @@
+package projectmp.client.gui;
+
+import projectmp.common.util.sidedictation.Side;
+import projectmp.common.util.sidedictation.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class GuiHandler {
+
+	
+	protected GuiHandler(){
+		
+	}
+	
+}
---
+++ core/src/projectmp/client/gui/RegisteredGuis.java
@@ -0,0 +1,31 @@
+package projectmp.client.gui;
+
+import java.util.HashMap;
+
+import projectmp.common.util.sidedictation.Side;
+import projectmp.common.util.sidedictation.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class RegisteredGuis {
+	
+	private static RegisteredGuis instance;
+
+	private RegisteredGuis() {
+	}
+
+	public static RegisteredGuis instance() {
+		if (instance == null) {
+			instance = new RegisteredGuis();
+			instance.loadResources();
+		}
+		return instance;
+	}
+	
+	private HashMap<String, GuiHandler> map = new HashMap<String, GuiHandler>();
+
+	private void loadResources() {
+		
+		
+	}
+	
+}
