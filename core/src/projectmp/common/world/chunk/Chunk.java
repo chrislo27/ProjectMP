c702b59cf85ac91995f160ef6d789944234753ad
chrislo27 <kinchrislo@gmail.com>
2015-05-16 13:16:04 -0700

    Chunk now writes TileEntities to NBT

--- core/src/projectmp/common/world/chunk/Chunk.java
+++ core/src/projectmp/common/world/chunk/Chunk.java
@@ -3,34 +3,36 @@ package projectmp.common.world.chunk;
 import projectmp.common.block.Block;
 import projectmp.common.block.Blocks;
 import projectmp.common.nbt.NBTIOAble;
+import projectmp.common.tileentity.TileEntity;
 
 import com.evilco.mc.nbt.tag.TagByteArray;
 import com.evilco.mc.nbt.tag.TagCompound;
 import com.evilco.mc.nbt.tag.TagIntegerArray;
 
-
-public class Chunk implements NBTIOAble{
+public class Chunk implements NBTIOAble {
 
 	public static final int CHUNK_SIZE = 16;
-	
+
 	protected Block[][] blocks = new Block[CHUNK_SIZE][CHUNK_SIZE];
 	protected byte[][] metadata = new byte[CHUNK_SIZE][CHUNK_SIZE];
-	
+	protected TileEntity[][] tileEntities = new TileEntity[CHUNK_SIZE][CHUNK_SIZE];
+
 	int locationX = 0;
 	int locationY = 0;
-	
-	public Chunk(int locx, int locy){
+
+	public Chunk(int locx, int locy) {
 		locationX = locx;
 		locationY = locy;
-		
-		for(int x = 0; x < CHUNK_SIZE; x++){
-			for(int y = 0; y < CHUNK_SIZE; y++){
+
+		for (int x = 0; x < CHUNK_SIZE; x++) {
+			for (int y = 0; y < CHUNK_SIZE; y++) {
 				blocks[x][y] = Blocks.defaultBlock();
 				metadata[x][y] = 0;
+				tileEntities[x][y] = null;
 			}
 		}
 	}
-	
+
 	public Block getBlock(int x, int y) {
 		if (x < 0 || y < 0 || x >= CHUNK_SIZE || y >= CHUNK_SIZE) return Blocks.defaultBlock();
 		if (blocks[x][y] == null) return Blocks.defaultBlock();
@@ -40,6 +42,11 @@ public class Chunk implements NBTIOAble{
 	public int getMeta(int x, int y) {
 		if (x < 0 || y < 0 || x >= CHUNK_SIZE || y >= CHUNK_SIZE) return 0;
 		return metadata[x][y];
+	}
+
+	public TileEntity geTileEntity(int x, int y) {
+		if (x < 0 || y < 0 || x >= CHUNK_SIZE || y >= CHUNK_SIZE) return null;
+		return tileEntities[x][y];
 	}
 
 	public void setBlock(Block b, int x, int y) {
@@ -56,23 +63,31 @@ public class Chunk implements NBTIOAble{
 	public TagCompound writeToNBT(TagCompound tag) {
 		int[] blockids = new int[CHUNK_SIZE * CHUNK_SIZE];
 		byte[] metas = new byte[CHUNK_SIZE * CHUNK_SIZE];
-		for(int x = 0; x < CHUNK_SIZE; x++){
-			for(int y = 0; y < CHUNK_SIZE; y++){
+		TagCompound tiles = new TagCompound("TileEntities");
+		
+		for (int x = 0; x < CHUNK_SIZE; x++) {
+			for (int y = 0; y < CHUNK_SIZE; y++) {
 				blockids[(x * CHUNK_SIZE) + y] = Blocks.instance().getIDFromBlock(blocks[x][y]);
 				metas[(x * CHUNK_SIZE) + y] = metadata[x][y];
+				if(tileEntities[x][y] != null){
+					TagCompound teTag = new TagCompound("TileEntity_" + x + "," + y);
+					tileEntities[x][y].writeToNBT(teTag);
+					tiles.setTag(teTag);
+				}
 			}
 		}
-		
-		tag.setTag(new TagIntegerArray("Location", new int[]{locationX, locationY}));
+
+		tag.setTag(new TagIntegerArray("Location", new int[] { locationX, locationY }));
 		tag.setTag(new TagIntegerArray("Blocks", blockids));
 		tag.setTag(new TagByteArray("Metadata", metas));
-		
+		tag.setTag(tiles);
+
 		return tag;
 	}
 
 	@Override
 	public void readFromNBT(TagCompound tag) {
-		
+
 	}
-	
+
 }
