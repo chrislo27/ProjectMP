127c404e4eacc4eece1bd5f54c58dbb2f13eefb9
chrislo27 <kinchrislo@gmail.com>
2015-06-07 15:01:38 -0700

    Override for addAnimation

--- core/src/projectmp/common/item/Item.java
+++ core/src/projectmp/common/item/Item.java
@@ -1,10 +1,18 @@
 package projectmp.common.item;
 
+import projectmp.client.animation.LoopingAnimation;
 import projectmp.common.TexturedObject;
 
 
 public class Item extends TexturedObject{
 
 	String name = "unnamed";
+	
+	@Override
+	public Item addAnimation(LoopingAnimation a){
+		super.addAnimation(a);
+		
+		return this;
+	}
 	
 }
