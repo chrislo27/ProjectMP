1ff83f8e4c5bc5a18faed9a9a9baf09801bcbfda
chrislo27 <kinchrislo@gmail.com>
2015-06-08 16:26:56 -0700

    Added locale for items

--- core/src/projectmp/common/item/Item.java
+++ core/src/projectmp/common/item/Item.java
@@ -2,17 +2,26 @@ package projectmp.common.item;
 
 import projectmp.client.animation.LoopingAnimation;
 import projectmp.common.TexturedObject;
+import projectmp.common.Translator;
 
 
 public class Item extends TexturedObject{
 
 	String name = "unnamed";
 	
+	public Item(String unlocalizedname){
+		name = unlocalizedname;
+	}
+	
 	@Override
 	public Item addAnimation(LoopingAnimation a){
 		super.addAnimation(a);
 		
 		return this;
+	}
+	
+	public String getLocalizedName(){
+		return Translator.instance().getMsg("item." + name);
 	}
 	
 }
