e00529c643a48534cb8d3c6f2793d4f3361b12f6
chrislo27 <kinchrislo@gmail.com>
2015-06-14 21:39:39 -0700

    Maybe the inventory packet should have an inventory field

--- core/src/projectmp/common/packet/PacketSendInv.java
+++ core/src/projectmp/common/packet/PacketSendInv.java
@@ -1,12 +1,15 @@
 package projectmp.common.packet;
 
 import projectmp.client.ClientLogic;
+import projectmp.common.inventory.Inventory;
 import projectmp.server.ServerLogic;
 
 import com.esotericsoftware.kryonet.Connection;
 
 public class PacketSendInv implements Packet {
 
+	Inventory inv = null;
+	
 	@Override
 	public void actionServer(Connection connection, ServerLogic logic) {
 	}
