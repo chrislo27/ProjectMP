bc39cb7720a2896ef25d50b425348cb4ba679c57
chrislo27 <kinchrislo@gmail.com>
2015-06-16 20:38:33 -0700

    More work on #17 - packet ClickSlot has fields
    Don't judge me I have finals this week

--- core/src/projectmp/common/packet/PacketClickSlot.java
+++ core/src/projectmp/common/packet/PacketClickSlot.java
@@ -8,6 +8,9 @@ import com.esotericsoftware.kryonet.Connection;
 
 public class PacketClickSlot implements Packet{
 
+	public String guiId = null;
+	public int slotId = -999; // -1 is reserved for mouse cursor
+	
 	@Override
 	public void actionServer(Connection connection, ServerLogic logic) {
 	}
