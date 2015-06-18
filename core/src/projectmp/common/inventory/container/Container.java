83a38fa4aebc8ab6c2c6e8ddaf08b66d45a1f18a
chrislo27 <kinchrislo@gmail.com>
2015-06-18 09:12:23 -0700

    Documentation

--- core/src/projectmp/common/inventory/container/Container.java
+++ core/src/projectmp/common/inventory/container/Container.java
@@ -10,7 +10,7 @@ import com.badlogic.gdx.utils.Array;
 /**
  * A container connects the player's inventory and a tile entity's inventory to a GUI. It has the slot instances.
  * <br>
- * This is re-instantiated whenever a player opens an inventory.
+ * This is re-instantiated whenever a player opens an inventory on both client (in the Gui class) and server.
  * <br>
  * All sub-classes should add slots in their constructor.
  *
