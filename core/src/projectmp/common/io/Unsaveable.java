4bb84b651d30620367fd77e91f577ae8e09e47b6
chrislo27 <kinchrislo@gmail.com>
2015-07-04 21:23:41 -0700

    Added documentation to Unsaveable

--- core/src/projectmp/common/io/Unsaveable.java
+++ core/src/projectmp/common/io/Unsaveable.java
@@ -1,5 +1,10 @@
 package projectmp.common.io;
 
+/**
+ * Dictates that this item should not be written when the world is being saved (entities, tile entities, etc.)
+ * 
+ *
+ */
 public interface Unsaveable {
 
 }
