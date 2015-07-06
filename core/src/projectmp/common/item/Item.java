e0294b77135675199431d8668311b435b034f9ee
chrislo27 <kinchrislo@gmail.com>
2015-07-06 15:07:35 -0700

    Item usage methods require EntityPlayer instead of Entity

--- core/src/projectmp/common/item/Item.java
+++ core/src/projectmp/common/item/Item.java
@@ -4,7 +4,7 @@ import projectmp.client.WorldRenderer;
 import projectmp.client.animation.Animation;
 import projectmp.common.TexturedObject;
 import projectmp.common.Translator;
-import projectmp.common.entity.Entity;
+import projectmp.common.entity.EntityPlayer;
 import projectmp.common.inventory.itemstack.ItemStack;
 import projectmp.common.world.World;
 
@@ -18,7 +18,7 @@ public class Item extends TexturedObject {
 		this.unlocalizedName = unlocalizedname;
 	}
 
-	public void onUseStart(World world, Entity user){
+	public void onUseStart(World world, EntityPlayer user){
 		
 	}
 	
@@ -27,11 +27,11 @@ public class Item extends TexturedObject {
 	 * @param world
 	 * @param user
 	 */
-	public void onUsing(World world, Entity user){
+	public void onUsing(World world, EntityPlayer user){
 		
 	}
 	
-	public void onUseEnd(World world, Entity user){
+	public void onUseEnd(World world, EntityPlayer user){
 		
 	}
 	
