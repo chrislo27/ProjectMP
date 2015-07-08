15b76e8bb10315406142b04e04fb2da9c1cec595
chrislo27 <kinchrislo@gmail.com>
2015-07-08 15:10:35 -0700

    Extension is .dat

--- core/src/projectmp/common/util/FileNameUtils.java
+++ core/src/projectmp/common/util/FileNameUtils.java
@@ -8,7 +8,7 @@ public final class FileNameUtils {
 	/**
 	 * includes the dot
 	 */
-	public static final String FILE_EXTENSION = "";
+	public static final String FILE_EXTENSION = ".dat";
 	public static final String DEFAULT_SAVE_FOLDER = "saves/";
 	public static final String WORLD_FILE_NAME = "world";
 	public static final String PLAYER_FILE_NAME = "players";
