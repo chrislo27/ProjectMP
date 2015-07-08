848ce8b4a561ec51d70acfb0b382ade15e4465a4
chrislo27 <kinchrislo@gmail.com>
2015-07-08 08:49:28 -0700

    Now implements Unsaveable again

--- core/src/projectmp/common/entity/EntityPlayer.java
+++ core/src/projectmp/common/entity/EntityPlayer.java
@@ -3,20 +3,19 @@ package projectmp.common.entity;
 import projectmp.client.WorldRenderer;
 import projectmp.common.inventory.Inventory;
 import projectmp.common.inventory.InventoryPlayer;
+import projectmp.common.io.Unsaveable;
 import projectmp.common.registry.AssetRegistry;
 import projectmp.common.tileentity.HasInventory;
-import projectmp.common.util.AssetMap;
 import projectmp.common.util.NBTUtils;
 import projectmp.common.util.Utils;
 import projectmp.common.world.World;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.evilco.mc.nbt.error.TagNotFoundException;
 import com.evilco.mc.nbt.error.UnexpectedTagTypeException;
 import com.evilco.mc.nbt.tag.TagCompound;
 import com.evilco.mc.nbt.tag.TagString;
 
-public class EntityPlayer extends EntityLiving implements HasInventory, ILoadsChunk {
+public class EntityPlayer extends EntityLiving implements HasInventory, ILoadsChunk, Unsaveable {
 
 	public String username = "UNKNOWN PLAYER NAME RAWR";
 	private InventoryPlayer inventory;
