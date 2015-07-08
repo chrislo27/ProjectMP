ace474c5f148032b7ba2bd340ca73240b5a95abc
chrislo27 <kinchrislo@gmail.com>
2015-07-08 08:48:54 -0700

    Added some fields

--- core/src/projectmp/common/packet/PacketItemUse.java
+++ core/src/projectmp/common/packet/PacketItemUse.java
@@ -1,13 +1,31 @@
 package projectmp.common.packet;
 
 import projectmp.client.ClientLogic;
+import projectmp.common.inventory.itemstack.ItemStack;
 import projectmp.server.ServerLogic;
 
 import com.esotericsoftware.kryonet.Connection;
 
-
+/**
+ * client sends this to the server
+ * 
+ *
+ */
 public class PacketItemUse implements Packet{
 
+	public static final int ON_START = 0;
+	public static final int ON_END = 1;
+	
+	/**
+	 * The status (start or end). By default ON_END (safer than ON_START).
+	 */
+	public int status = ON_END;
+	
+	/**
+	 * The item stack.
+	 */
+	public ItemStack stack = new ItemStack(null, 0);
+	
 	@Override
 	public void actionServer(Connection connection, ServerLogic logic) {
 	}
