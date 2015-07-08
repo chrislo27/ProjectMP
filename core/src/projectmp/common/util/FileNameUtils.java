77bf36a95b812a5be73baf0810cc7822f81321af
chrislo27 <kinchrislo@gmail.com>
2015-07-08 14:40:30 -0700

    Rename world name to default world name

--- core/src/projectmp/common/util/FileNameUtils.java
+++ core/src/projectmp/common/util/FileNameUtils.java
@@ -5,8 +5,11 @@ public final class FileNameUtils {
 	
 	private FileNameUtils(){}
 	
+	/**
+	 * includes the dot
+	 */
 	public static final String FILE_EXTENSION = "";
-	public static final String SAVE_FOLDER = "saves/";
+	public static final String DEFAULT_SAVE_FOLDER = "saves/";
 	public static final String WORLD_FILE_NAME = "world";
 	public static final String PLAYER_FILE_NAME = "players";
 	
