2cc5d9cec342b16986bf11520305e1603e9febd9
chrislo27 <kinchrislo@gmail.com>
2015-07-08 14:38:46 -0700

    Added static final strings for file extensions and whatnot

---
+++ core/src/projectmp/common/util/FileNameUtils.java
@@ -0,0 +1,13 @@
+package projectmp.common.util;
+
+
+public final class FileNameUtils {
+	
+	private FileNameUtils(){}
+	
+	public static final String FILE_EXTENSION = "";
+	public static final String SAVE_FOLDER = "saves/";
+	public static final String WORLD_FILE_NAME = "world";
+	public static final String PLAYER_FILE_NAME = "players";
+	
+}
