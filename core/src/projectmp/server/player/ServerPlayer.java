4d3632121738256628f0f902b720343b5e005d0d
chrislo27 <kinchrislo@gmail.com>
2015-07-08 09:45:37 -0700

    Completed NBT writing/reading methods

--- core/src/projectmp/server/player/ServerPlayer.java
+++ core/src/projectmp/server/player/ServerPlayer.java
@@ -1,11 +1,12 @@
 package projectmp.server.player;
 
+import projectmp.common.inventory.InventoryPlayer;
+import projectmp.common.io.CanBeSavedToNBT;
+
 import com.evilco.mc.nbt.error.TagNotFoundException;
 import com.evilco.mc.nbt.error.UnexpectedTagTypeException;
 import com.evilco.mc.nbt.tag.TagCompound;
-
-import projectmp.common.inventory.InventoryPlayer;
-import projectmp.common.io.CanBeSavedToNBT;
+import com.evilco.mc.nbt.tag.TagFloat;
 
 /**
  * Only used by the server instance to keep track of player position, inventory, etc. This is also why EntityPlayer implements Unsaveable.
@@ -23,13 +24,27 @@ public class ServerPlayer implements CanBeSavedToNBT{
 		this.username = username;
 	}
 
+	/**
+	 * The tag's name is also the username
+	 */
 	@Override
 	public void writeToNBT(TagCompound tag) {
+		TagCompound inv = new TagCompound("Inventory");
+		inventory.writeToNBT(inv);
+		tag.setTag(inv);
+		
+		tag.setTag(new TagFloat("PosX", posx));
+		tag.setTag(new TagFloat("PosY", posy));
 	}
 
 	@Override
 	public void readFromNBT(TagCompound tag) throws TagNotFoundException,
 			UnexpectedTagTypeException {
+		inventory.readFromNBT(tag.getCompound("Inventory"));
+		username = tag.getName();
+		
+		posx = tag.getFloat("PosX");
+		posy = tag.getFloat("PosY");
 	}
 	
 }
