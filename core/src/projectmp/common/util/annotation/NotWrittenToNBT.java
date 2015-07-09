939ca12a199cb5449a3cf9b440de1f481eb4b4fc
chrislo27 <kinchrislo@gmail.com>
2015-07-08 17:13:28 -0700

    Added annotation for source that shows a field isn't NBTable

---
+++ core/src/projectmp/common/util/annotation/NotWrittenToNBT.java
@@ -0,0 +1,16 @@
+package projectmp.common.util.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Shows a field doesn't get written to NBT if the containing class implements CanBeSavedToNBT
+ * 
+ *
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotWrittenToNBT {
+	
+	
+	
+}
