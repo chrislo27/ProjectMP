be64d17a304a5bd7c43018645e9d61e247a02e9a
chrislo27 <kinchrislo@gmail.com>
2015-07-08 17:19:55 -0700

    Added current item using field and ticking

--- core/src/projectmp/server/player/ServerPlayer.java
+++ core/src/projectmp/server/player/ServerPlayer.java
@@ -1,7 +1,10 @@
 package projectmp.server.player;
 
 import projectmp.common.inventory.InventoryPlayer;
+import projectmp.common.inventory.itemstack.ItemStack;
 import projectmp.common.io.CanBeSavedToNBT;
+import projectmp.common.util.annotation.NotWrittenToNBT;
+import projectmp.server.ServerLogic;
 
 import com.evilco.mc.nbt.error.TagNotFoundException;
 import com.evilco.mc.nbt.error.UnexpectedTagTypeException;
@@ -20,10 +23,39 @@ public class ServerPlayer implements CanBeSavedToNBT{
 	public float posx;
 	public float posy;
 	
+	@NotWrittenToNBT
+	private ItemStack currentUsingItem = null;
+	
 	public ServerPlayer(String username){
 		this.username = username;
 	}
 
+	public void tickUpdate(ServerLogic logic){
+		if(isUsingItem()){
+			currentUsingItem.getItem().onUsing(logic.world, logic.getPlayerByName(username));
+		}
+	}
+	
+	public boolean isUsingItem(){
+		return currentUsingItem != null;
+	}
+	
+	public void stopUsingItem(ServerLogic logic){
+		if(!isUsingItem()) return;
+		if(currentUsingItem.getItem() == null) return;
+		
+		currentUsingItem.getItem().onUseEnd(logic.world, logic.getPlayerByName(username));
+	}
+	
+	public void startUsingItem(ServerLogic logic, ItemStack item){
+		if(isUsingItem()) stopUsingItem(logic);
+		if(item == null) return;
+		if(item.getItem() == null) return;
+		
+		currentUsingItem = item.copy();
+		currentUsingItem.getItem().onUseStart(logic.world, logic.getPlayerByName(username));
+	}
+	
 	/**
 	 * The tag's name is also the username
 	 */
