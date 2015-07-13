e724c4dfdaf3cebdaa5db9789d42ac88d79a0d86
chrislo27 <kinchrislo@gmail.com>
2015-07-12 19:50:48 -0700

    Added world map generator (only does air/non-air)

--- core/src/projectmp/common/generation/GenerationGroups.java
+++ core/src/projectmp/common/generation/GenerationGroups.java
@@ -1,8 +1,13 @@
 package projectmp.common.generation;
 
+import projectmp.common.block.Blocks;
 import projectmp.common.generation.terrain.TerrainLandscape;
 import projectmp.common.world.World;
 
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.utils.Array;
 
 /**
@@ -49,6 +54,24 @@ public class GenerationGroups {
 		for(int i = 0; i < instance().groups.size; i++){
 			instance().groups.get(i).generate(world, world.noiseGen);
 		}
+		
+		Pixmap pix = new Pixmap(world.sizex, world.sizey, Format.RGBA8888);
+		for(int x = 0; x < world.sizex; x++){
+			for(int y = 0; y < world.sizey; y++){
+				pix.setColor(1, 1, 1, 1);
+				pix.drawPixel(x, y);
+				
+				float noise = (float) world.noiseGen.eval(x * 0.05, y * 0.05);
+				noise = (noise + 1) / 2f;
+				
+				if(world.getBlock(x, y) != Blocks.instance().getBlock("empty")){
+					pix.setColor(0, 0, 0, 1);
+				}
+				pix.drawPixel(x, y);
+			}
+		}
+		PixmapIO.writePNG(new FileHandle("noisemaps/worldmap.png"), pix);
+		pix.dispose();
 	}
 	
 }
