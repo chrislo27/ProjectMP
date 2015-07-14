ed880acd0fceff00c95e85fbcdf0f151f01379ad
chrislo27 <kinchrislo@gmail.com>
2015-07-14 10:05:46 -0700

    Clients can send to server to update everyone else
    Clients are authorative for the mining items (because server doesn't
    have access to the cursor position)

--- core/src/projectmp/common/packet/PacketBreakingProgress.java
+++ core/src/projectmp/common/packet/PacketBreakingProgress.java
@@ -1,19 +1,25 @@
 package projectmp.common.packet;
 
 import projectmp.client.ClientLogic;
+import projectmp.common.packet.repository.PacketRepository;
 import projectmp.server.ServerLogic;
 
 import com.esotericsoftware.kryonet.Connection;
 
-
-public class PacketBreakingProgress implements Packet{
+public class PacketBreakingProgress implements Packet {
 
 	public int x, y;
 	public float progress;
-	
+
 	@Override
 	public void actionServer(Connection connection, ServerLogic logic) {
+		PacketBreakingProgress packet = PacketRepository.instance().breakingProgress;
+
+		packet.x = x;
+		packet.y = y;
+		packet.progress = logic.world.getBreakingProgress(x, y);
 		
+		logic.server.sendToAllExceptUDP(connection.getID(), packet);
 	}
 
 	@Override
