4ca81935b28f0fb8dfde645a19d16ee9d8524767
chrislo27 <kinchrislo@gmail.com>
2015-07-13 23:12:11 -0700

    Added test visual mining

--- core/src/projectmp/common/item/ItemMineable.java
+++ core/src/projectmp/common/item/ItemMineable.java
@@ -1,5 +1,12 @@
 package projectmp.common.item;
 
+import projectmp.common.Main;
+import projectmp.common.entity.EntityPlayer;
+import projectmp.common.inventory.itemstack.ItemStack;
+import projectmp.common.world.World;
+
+import com.badlogic.gdx.Gdx;
+
 
 /**
  * This item can break blocks by holding down the use key. 
@@ -10,6 +17,18 @@ public class ItemMineable extends Item{
 
 	public ItemMineable(String unlocalizedname) {
 		super(unlocalizedname);
+	}
+	
+	@Override
+	public void onUsing(World world, EntityPlayer player, ItemStack stack){
+		if(world.isServer == false){
+			int x = world.main.clientLogic.getCursorBlockX();
+			int y = world.main.clientLogic.getCursorBlockY();
+			
+			float progress = world.getBreakingProgress(x, y);
+			
+			world.setBreakingProgress(x, y, progress + 0.5f * Gdx.graphics.getDeltaTime());
+		}
 	}
 
 }
