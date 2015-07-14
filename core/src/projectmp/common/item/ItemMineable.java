f76b07784504b60f19067afcc8e1db789af841df
chrislo27 <kinchrislo@gmail.com>
2015-07-13 22:45:26 -0700

    Organized imports

--- core/src/projectmp/common/item/ItemMineable.java
+++ core/src/projectmp/common/item/ItemMineable.java
@@ -1,12 +1,5 @@
 package projectmp.common.item;
 
-import projectmp.client.WorldRenderer;
-import projectmp.common.inventory.itemstack.ItemStack;
-import projectmp.common.util.MathHelper;
-import projectmp.common.util.Utils;
-
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 /**
  * This item can break blocks by holding down the use key. 
