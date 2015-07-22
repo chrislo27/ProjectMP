3041d9201d6b34b8ce4a10816a1b8a518751f656
chrislo27 <kinchrislo@gmail.com>
2015-07-21 21:50:25 -0700

    Bettered

--- core/src/projectmp/common/util/render/LiquidContainer.java
+++ core/src/projectmp/common/util/render/LiquidContainer.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.MathUtils;
 
 public class LiquidContainer {
 
@@ -15,6 +16,7 @@ public class LiquidContainer {
 	 * Gap between vertices
 	 */
 	float waveGap = 4;
+	float maxPeak = 32;
 	Color color = new Color();
 
 	float[] waves;
@@ -23,16 +25,55 @@ public class LiquidContainer {
 	SimplexNoise noise = new SimplexNoise(System.currentTimeMillis());
 	float secondsElapsed = 0;
 
-	public LiquidContainer(float w, float h, float gap, Color c) {
+	public LiquidContainer(float w, float h, float gap, float peak, Color c) {
 		width = w;
 		height = h;
 		waveGap = gap;
 		color = c;
+		maxPeak = peak;
 
 		waves = new float[(int) Math.max(width / waveGap, 2f)];
-		for (int i = 0; i < waves.length; i++) {
-			waves[i] = 0;
+	}
+
+	protected void renderUpdate(Main main) {
+		secondsElapsed += Gdx.graphics.getDeltaTime();
+
+		if (Gdx.input.isKeyPressed(Keys.R)) {
+			for (int i = 0; i < waves.length; i++) {
+				perturb(MathUtils.random(), 2f);
+			}
 		}
+		
+		for(int i = 0; i < 4; i++){
+			smooth(main);
+		}
+	}
+	
+	private void smooth(Main main){
+		for (int i = 0; i < waves.length; i++) {
+			float old = waves[i];
+
+			if (i > 0) {
+				waves[i - 1] += old / 4;
+			}
+
+			if (i < waves.length - 1) {
+				waves[i + 1] += old / 4;
+			} else {
+				endWave += old / 4;
+			}
+
+			waves[i] /= 2;
+
+		}
+
+		waves[waves.length - 1] += endWave / 4;
+		endWave /= 2;
+	}
+
+	public void perturb(float x, float intensity) {
+		waves[(int) ((width * x) / waveGap)] += intensity * MathUtils.random()
+				* MathUtils.randomSign();
 	}
 
 	public void render(Main main, float x, float y) {
@@ -40,10 +81,12 @@ public class LiquidContainer {
 
 		main.verticesRenderer.begin(Main.camera.combined, GL20.GL_TRIANGLES);
 
-		float lowestPoint = endWave;
+		float lowestPoint = endWave * maxPeak;
 
 		for (int i = 0; i < waves.length; i++) {
-			if (waves[i] < lowestPoint) lowestPoint = waves[i];
+			if (waves[i] * maxPeak < lowestPoint) {
+				lowestPoint = waves[i] * maxPeak;
+			}
 		}
 
 		// first draw a rectangle where the the liquid doesn't have spikes
@@ -81,25 +124,27 @@ public class LiquidContainer {
 				main.verticesRenderer.vertex(x, y + height + lowestPoint, 0);
 			} else {
 				// one before it
-				main.verticesRenderer.vertex(x + xPos - waveGap, y + height + waves[i - 1], 0);
+				main.verticesRenderer.vertex(x + xPos - waveGap, y + height + waves[i - 1]
+						* maxPeak, 0);
 			}
 
 			// center
 			main.verticesRenderer.color(color);
-			main.verticesRenderer.vertex(x + xPos, y + height + vertex, 0);
+			main.verticesRenderer.vertex(x + xPos, y + height + vertex * maxPeak, 0);
 
 			// after
 			main.verticesRenderer.color(color);
 			if (i >= waves.length - 1) {
-				main.verticesRenderer.vertex(x + xPos + waveGap, y + height + endWave, 0);
+				main.verticesRenderer.vertex(x + xPos + waveGap, y + height + endWave * maxPeak, 0);
 			} else {
-				main.verticesRenderer.vertex(x + xPos + waveGap, y + height + waves[i + 1], 0);
+				main.verticesRenderer.vertex(x + xPos + waveGap, y + height + waves[i + 1]
+						* maxPeak, 0);
 			}
 
 			// filling
 
 			main.verticesRenderer.color(color);
-			main.verticesRenderer.vertex(x + xPos, y + height + vertex, 0);
+			main.verticesRenderer.vertex(x + xPos, y + height + vertex * maxPeak, 0);
 
 			main.verticesRenderer.color(color);
 			main.verticesRenderer.vertex(x + xPos - waveGap + (i == 0 ? waveGap : 0), y + height
@@ -110,52 +155,37 @@ public class LiquidContainer {
 					x + xPos + waveGap - (i == waves.length - 1 ? waveGap : 0), y + height
 							+ lowestPoint, 0);
 
-			//if(i == waves.length - 1) continue;
-
 			main.verticesRenderer.color(color);
-			main.verticesRenderer.vertex(x + xPos, y + height + vertex, 0);
+			main.verticesRenderer.vertex(x + xPos, y + height + vertex * maxPeak, 0);
 
 			main.verticesRenderer.color(color);
 			main.verticesRenderer.vertex(x + xPos + (waveGap / 2f), y + height + lowestPoint, 0);
 
 			main.verticesRenderer.color(color);
 			main.verticesRenderer.vertex(x + xPos + waveGap, y + height
-					+ (i == waves.length - 1 ? endWave : waves[i + 1]), 0);
+					+ (i == waves.length - 1 ? endWave : waves[i + 1]) * maxPeak, 0);
 		}
 
 		main.verticesRenderer.color(color);
-		main.verticesRenderer.vertex(x + width, y + height + endWave, 0);
-		
+		main.verticesRenderer.vertex(x + width, y + height + endWave * maxPeak, 0);
+
 		main.verticesRenderer.color(color);
 		main.verticesRenderer.vertex(x + width, y + height + lowestPoint, 0);
-		
+
 		main.verticesRenderer.color(color);
 		main.verticesRenderer.vertex(x + width - waveGap / 2, y + height + lowestPoint, 0);
-		
+
 		main.verticesRenderer.color(color);
-		main.verticesRenderer.vertex(x + width - waveGap, y + height + waves[waves.length - 1], 0);
-		
+		main.verticesRenderer.vertex(x + width - waveGap, y + height + waves[waves.length - 1]
+				* maxPeak, 0);
+
 		main.verticesRenderer.color(color);
 		main.verticesRenderer.vertex(x + width - waveGap, y + height + lowestPoint, 0);
-		
+
 		main.verticesRenderer.color(color);
 		main.verticesRenderer.vertex(x + width - waveGap / 2, y + height + lowestPoint, 0);
 
 		main.verticesRenderer.end();
-	}
-
-	protected void renderUpdate(Main main) {
-		if (Gdx.input.isKeyPressed(Keys.R)) secondsElapsed += Gdx.graphics.getDeltaTime();
-
-		for (int i = 0; i < waves.length + 1; i++) {
-			float value = (float) (noise.eval(i * 0.2f - (secondsElapsed * 2f), 64) * 32f);
-
-			if (i == waves.length) {
-				endWave = value;
-			} else {
-				waves[i] = value;
-			}
-		}
 	}
 
 }
