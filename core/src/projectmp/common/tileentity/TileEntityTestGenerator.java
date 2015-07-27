e188d774e5f68861fe300330e4744bf796c8cf95
chrislo27 <kinchrislo@gmail.com>
2015-07-26 19:05:35 -0700

    Actually makes power

--- core/src/projectmp/common/tileentity/TileEntityTestGenerator.java
+++ core/src/projectmp/common/tileentity/TileEntityTestGenerator.java
@@ -4,6 +4,9 @@ import com.evilco.mc.nbt.error.TagNotFoundException;
 import com.evilco.mc.nbt.error.UnexpectedTagTypeException;
 import com.evilco.mc.nbt.tag.TagCompound;
 
+import projectmp.common.Main;
+import projectmp.common.Settings;
+import projectmp.common.energy.EnergyChain;
 import projectmp.common.world.World;
 
 
@@ -19,7 +22,9 @@ public class TileEntityTestGenerator extends TileEntityPowerHandler{
 	
 	@Override
 	public void tickUpdate(World world){
+		if(world.time.totalTicks % Main.TICKS != 0) return;
 		
+		EnergyChain.doSpreadPower(16, x, y, world);
 	}
 	
 }
